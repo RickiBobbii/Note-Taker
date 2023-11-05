83a955b3de39645c8160d3dd54961bcda6124792
RickiBobbii <rickibobbiii@gmail.com>
2023-11-05 15:50:37 -0500

    added post request to api

--- routes/notes-api.js
+++ routes/notes-api.js
@@ -7,6 +7,23 @@ router.get('/api/notes', (req,res) => {
     readFromFile('db/db.json').then((data) => res.json(JSON.parse(data)))
 });
 
-
+//
+router.post('/api/notes', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+      };
+  
+      readAndAppend(newNote, 'db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
 module.exports = router;
